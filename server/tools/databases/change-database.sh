af8f253b7b3f229bfe5175a858e2f1f444123653
Gregory Hall <gregoryhall994@gmail.com>
2018-09-20 11:14:34 -0600

    merge Bob's changes from old repo commit

--- server/tools/databases/change-database.sh
+++ server/tools/databases/change-database.sh
@@ -2,6 +2,13 @@
 
 DB_VENDOR=$1
 
+echo "========================================================================="
+echo ""
+echo "  Changing to $DB_VENDOR database"
+echo ""
+echo "========================================================================="
+echo ""
+
 cd /opt/jboss/keycloak
 
 bin/jboss-cli.sh --file=/opt/jboss/tools/cli/databases/$DB_VENDOR/standalone-configuration.cli
